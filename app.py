e64c4c000fdb6b3bbde182b3718bde0331211bde
Jorge Barreto <jsambarret0@gmail..com>
2022-11-17 13:47:12 -0300

    CHANGE: Drive Connection

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 #from aioflask import Flask, render_template
 from flask import Flask,jsonify, render_template
-from pega_cotacao import pega_cotacao
+from pega_cotacao import ler_drive, ler_json, pega_cotacao
 
 app = Flask(__name__)
 
 @app.route('/')
 def api_cotacao(lista=None):
-    lista = pega_cotacao("Empresas.xlsx")
+    lista = pega_cotacao()
     return jsonify(lista)
 
 @app.route('/grafico')
 def grafico():
     return render_template('index.html')
+
+@app.route('/lista')
+def lista(lista=None):
+    lista = ler_json()
+    return render_template('index.html', lista = lista)
+
+@app.route('/new_lista')
+def newLista(lista=None):
+    lista = ler_drive()
+    return jsonify(lista)#render_template('index.html', lista = lista)
--- pega_cotacao.py
+++ pega_cotacao.py
@@ -3,15 +3,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import date, timedelta 
 import matplotlib
+import json
+
+from spreedsheet import conecta_drive 
+
 matplotlib.use('SVG')
 
-def pega_cotacao(arq: str):
+def pega_cotacao():#arq: str
     today = date.today()
     yesterday = today - timedelta(days=1)
     one_week_ago = today - timedelta(days=7)
     thirty_days_ago = today - timedelta(days=30)
 
-    tabela_empresas = pd.read_excel(arq)
+    tabela_empresas = pd.DataFrame(ler_drive())#pd.read_excel(arq)
     cotacaoDiaLista = {}
     for empresa in tabela_empresas['Empresas']:
         cotacao = web.DataReader(f'{empresa}.SA', data_source='yahoo',start=thirty_days_ago, end=today) #mes/dia/ano
@@ -21,4 +25,17 @@ def pega_cotacao(arq: str):
         normalized_df=(cotacao-cotacao.mean())/cotacao.std()
         normalized_df["Adj Close"].plot(figsize=(10,8), label=empresa, linewidth=4).legend()
     plt.savefig('static/grafico.svg')
+    json_object = json.dumps(cotacaoDiaLista, indent = 4) 
+    with open("static/data/lista.json", "w") as outfile: 
+        outfile.write(json_object)
+    outfile.close()
     return cotacaoDiaLista
+
+def ler_json():
+    with open('static/data/lista.json', 'r') as openfile:  
+        json_object = json.load(openfile)
+    return json_object
+
+def ler_drive():
+    json_object = conecta_drive()
+    return json_object
